d13d7697f0633930a6a4507af311b4c9335ee0eb
iyuei <13142322+iyuei@user.noreply.gitee.com>
2024-07-09 09:09:09 +0800

    Update index.vue

--- Arco-Hub/src/views/login/index.vue
+++ Arco-Hub/src/views/login/index.vue
@@ -117,8 +117,8 @@ const [loginCode, setLoginCode] = useBasicsState(
 );
 // form
 const { form } = useForm(() => ({
-	userAccount: 'JCY',
-	password: 'JCY123',
+	userAccount: 'ZJX',
+	password: 'CS123456',
 	validateCode: '',
 }));
 // 按钮是否可点击
